105e6184a43338e36b6bd8acc0aba754d2778029
Seyeon Weon <sweon@users.noreply.github.com>
2015-09-30 06:10:52 +0900

    Fix subtitle loading problem

--- src/js/open-video.js
+++ src/js/open-video.js
@@ -2,61 +2,61 @@ var packaged_app = (window.location.origin.indexOf("chrome-extension") == 0);
 
 (function($) {
     $.extend(MediaElementPlayer.prototype, {
-	buildsource: function(player, controls, layers, media) {
-	    var 
-	    t = this,
-	    openFileInput = $('<input style="display:none" type="file" multiple id="openfile_input"/>')
-		.appendTo(controls);
-	    t.openedFile = null;
-	    var open  = 
-		$('<div class="mejs-button mejs-source-button mejs-source" >' +
-		  '<button type="button" aria-controls="' + t.id + '" title="' + mejs.i18n.t('Open video...') + '" aria-label="' + mejs.i18n.t('Open video...') + '"></button>' +
-		  '</div>')
-		.appendTo(controls);
-	    player.openFileForm = function () {
-		if (packaged_app) {
-		    chrome.fileSystem.chooseEntry({
-			type: "openFile"
-		    }, function (entry) {
-			entry.file(function fff(file) {
-			    media.stop();
-			    player.tracks = [];			    
-			    var path = window.URL.createObjectURL(file);
-			    t.openedFile = file;
-			    t.openedFileEntry = entry;
-			    mainMediaElement.setSrc(path);
-			    mainMediaElement.play();
-			});
-		    });
-		}
-		else {
-		    openFileInput[0].click();
-		}
-	    };
-	    open.click(function(e) {
-		e.preventDefault();
-		player.openFileForm();
-		return false;
-	    });
-	    openFileInput.change(function (e) {
-		media.stop();
-		player.tracks = [];
-        if (openFileInput[0].files[0].type.indexOf("subrip") >= 0) {
-            player.openSrtEntry(openFileInput[0].files[0]);
-		} else {
-			var path = window.URL.createObjectURL(openFileInput[0].files[0]);
-			t.openedFile = openFileInput[0].files[0];
-			media.setSrc(path);
-			window.history.pushState(null, null, "#"+t.openedFile.name);
-			player.openSrtEntry(openFileInput[0].files[1]);
-			media.play();
-			media.pause();
-			if (player.options.alwaysShowControls == false) {
-				player.startControlsTimer();	            
-			}
-		}
-		return false;
-	    });
-	}
+        buildsource: function(player, controls, layers, media) {
+            var 
+            t = this,
+                openFileInput = $('<input style="display:none" type="file" multiple id="openfile_input"/>')
+            .appendTo(controls);
+            t.openedFile = null;
+            var open  = 
+                $('<div class="mejs-button mejs-source-button mejs-source" >' +
+                  '<button type="button" aria-controls="' + t.id + '" title="' + mejs.i18n.t('Open video...') + '" aria-label="' + mejs.i18n.t('Open video...') + '"></button>' +
+                  '</div>')
+            .appendTo(controls);
+            player.openFileForm = function () {
+                if (packaged_app) {
+                    chrome.fileSystem.chooseEntry({
+                        type: "openFile"
+                    }, function (entry) {
+                        entry.file(function fff(file) {
+                            media.stop();
+                            player.tracks = [];			    
+                            var path = window.URL.createObjectURL(file);
+                            t.openedFile = file;
+                            t.openedFileEntry = entry;
+                            mainMediaElement.setSrc(path);
+                            mainMediaElement.play();
+                        });
+                    });
+                }
+                else {
+                    openFileInput[0].click();
+                }
+            };
+            open.click(function(e) {
+                e.preventDefault();
+                player.openFileForm();
+                return false;
+            });
+            openFileInput.change(function (e) {
+                media.stop();
+                player.tracks = [];
+                if (openFileInput[0].files[0].type.indexOf("subrip") >= 0) {
+                    player.openSrtEntry(openFileInput[0].files[0]);
+                } else {
+                    var path = window.URL.createObjectURL(openFileInput[0].files[0]);
+                    t.openedFile = openFileInput[0].files[0];
+                    media.setSrc(path);
+                    player.openSrtEntry(openFileInput[0].files[1]);
+                    media.play();
+                    window.history.pushState(null, null, "#"+t.openedFile.name);
+                    media.pause();
+                    if (player.options.alwaysShowControls == false) {
+                        player.startControlsTimer();	            
+                    }
+                }
+                return false;
+            });
+        }
     });
 })(mejs.$);
