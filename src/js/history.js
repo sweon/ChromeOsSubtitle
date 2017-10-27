20649a1c76f184b3ecd50205a74918868f235631
Seyeon Weon <sweon@users.noreply.github.com>
2017-10-28 04:08:22 +0900

    Add history function.

---
+++ src/js/history.js
@@ -0,0 +1,75 @@
+var packaged_app = (window.location.origin.indexOf("chrome-extension") == 0);
+
+(function($) {
+    $.extend(MediaElementPlayer.prototype, {
+	buildhistory: function(player, controls, layers, media) {
+	    var t = this;
+	    var historyText = 
+		'<div class="me-window" style="color:#fff;margin: auto;position: absolute;top: 0; left: 0; bottom: 0; right: 0;width:650px;display: table; height: auto;background: url(background.png);background: rgba(50,50,50,0.7);border: solid 1px transparent;padding: 10px;overflow: hidden;-webkit-border-radius: 0;-moz-border-radius: 0;border-radius: 0;font-size: 16px;visibility: hidden;"></div>';
+
+	    var history = $(historyText
+	    ).appendTo(controls[0].parentElement);
+
+	    function hideHistory(e) {
+		history.css('visibility','hidden');
+		if (player.media.paused)
+		    $(".mejs-overlay-play").show();
+		
+		e.preventDefault();
+		e.stopPropagation();
+		player.container.off("click", hideHistory);
+		return false;
+	    }
+
+	    t.openHistoryWindow = function() {
+
+        var srtFiles = {};
+        var historyFiles = '<ol>';
+        for (var key in player.history) {
+            var file = player.history[key];
+            srtFiles[key] = file.srtFile;
+            historyFiles = historyFiles + '<li><a href="' + file.path + '" data-currentTime="' + file.currentTime + '" style="text-decoration: none;">' + file.file + '</li>';
+        }
+        historyFiles = historyFiles + '</ol>';
+
+        history.html(historyFiles);
+
+        history.find("a").click(function (e) {
+            player.history[document.title].currentTime = media.currentTime;
+            var currentTime = this.getAttribute('data-currentTime');
+            t.openedFile = this.innerText;
+            media.setSrc(this.href);
+            var srtFile = srtFiles[this.innerText];
+            player.openSrtEntry(srtFile);
+            media.setCurrentTime(currentTime);
+            // media.play();
+            // media.pause();
+            if (player.options.alwaysShowControls == false) {
+                player.startControlsTimer();                
+            };
+            document.title = t.openedFile;
+
+            // event.stopPropagation();
+            // return false;
+        });
+
+		$('.me-window').css('visibility','hidden');
+		history.css('visibility','visible');
+		$(".mejs-overlay-play").hide();
+		player.container.click(hideHistory);
+	    };
+
+        var open  = 
+            $('<div class="mejs-button mejs-history-button mejs-history" >' +
+              '<button type="button" aria-controls="' + t.id + '" title="' + mejs.i18n.t('Open video...') + '" aria-label="' + mejs.i18n.t('Open history...') + '"></button>' +
+              '</div>')
+        .prependTo(controls)
+        .click(function(e) {
+            e.preventDefault();
+            t.openHistoryWindow();
+            return false;
+        });
+
+	}
+    });
+})(mejs.$);
